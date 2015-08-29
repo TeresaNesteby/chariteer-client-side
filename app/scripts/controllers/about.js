1a9781e3988ccf27dc293dd9e27b1fa88502c609
TeresaNesteby <teresa.nesteby@gmail.com>
2015-08-28 23:14:45 -0700

    init commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name chariteerAngularApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the chariteerAngularApp
+ */
+angular.module('chariteerAngularApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name chariteerAngularApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the chariteerAngularApp
+ */
+angular.module('chariteerAngularApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
